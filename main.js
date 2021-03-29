b1e75ac4ac5ba2779ffb6146cd877a8f0ce4e3fb
Jahan Rashidi <56563402+ImNotJahan@users.noreply.github.com>
2021-03-29 08:40:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ function addUsernameEntry()
   usernameEntries.push([usernameInput.value, ipInput.value]);
 }
 
+var messages = document.getElementById("message-text");
+
 function grabMessages()
 {
-
+  (async () => {
+    const response = await fetch("imnotjahan.github.io");
+    const text = await response.text();
+    
+    messages.innerHTML = text;
+  })();
 }
+
+grabMessages();

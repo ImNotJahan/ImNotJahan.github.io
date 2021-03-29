5556cbeea3f03bd99c7271fd1bcc70e8eaef3cfe
Jahan Rashidi <56563402+ImNotJahan@users.noreply.github.com>
2021-03-29 08:24:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var usernameInput = document.getElementById("username");
+var ipInput = document.getElementById("ip");
+
+var usernameEntries = [];
+
+function addUsernameEntry()
+{
+  usernameEntries.push([usernameInput.value, ipInput.value]);
+}
+
+function grabMessages()
+{
+
+}
